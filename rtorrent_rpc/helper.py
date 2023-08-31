1cb0fd27e4fc728810b6cb55ddf35d894e8e737f
Trim21 <trim21.me@gmail.com>
2023-08-31 23:10:38 +0800

    fix: handle `FileNotFoundError`

--- rtorrent_rpc/helper.py
+++ rtorrent_rpc/helper.py
@@ -45,8 +45,11 @@ def add_completed_resume_file(base_save_path: Path, torrent_content: bytes) -> b
             else:
                 files.append({b"complete": 0, b"mtime": 0, b"priority": 1})
     else:
-        # return torrent_content
-        stat = base_save_path.joinpath(data[b"info"][b"name"].decode()).lstat()
+        try:
+            stat = base_save_path.joinpath(data[b"info"][b"name"].decode()).lstat()
+        except FileNotFoundError:
+            return torrent_content
+
         if stat.st_size == data[b"info"][b"length"]:
             files.append(
                 {b"complete": piece_count, b"mtime": int(stat.st_mtime), b"priority": 1}
