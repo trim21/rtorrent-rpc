70e679f351c7d103977217d9c768a87afcac0805
Trim21 <trim21.me@gmail.com>
2024-04-25 20:25:36 +0800

    fix

--- e2e/test_e2e.py
+++ e2e/test_e2e.py
@@ -5,7 +5,7 @@ from rtorrent_rpc import RTorrent
 
 def test_unix_path():
     p = Path(__file__).joinpath("../..").joinpath("tests/fixtures/run/rtorrent.sock")
-    t = RTorrent(p.resolve().as_posix().replace("file://", "scgi://"))
+    t = RTorrent("scgi://" + p.resolve().as_posix())
 
     assert t.system_list_methods()
     assert t.jsonrpc.call("system.listMethods", [])
