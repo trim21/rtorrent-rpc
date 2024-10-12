be9c709b322cecf5959e1372c21bf80a51b55c0a
Trim21 <trim21.me@gmail.com>
2024-10-13 01:11:24 +0800

    fix: bencode2

--- rtorrent_rpc/helper.py
+++ rtorrent_rpc/helper.py
@@ -90,17 +90,17 @@ def __add_resume_file(
     """
     based on [rtorrent_fast_resume.pl](https://github.com/rakshasa/rtorrent/blob/master/doc/rtorrent_fast_resume.pl)
     """
-    data: dict[str, Any] = bencode2.bdecode(torrent_content, str_key=True)
+    data: dict[bytes, Any] = bencode2.bdecode(torrent_content)
 
-    piece_length = data["info"]["piece length"]
-    files = []
+    piece_length = data[b"info"][b"piece length"]
+    files: list[dict[str, Any]] = []
 
-    t_files = data["info"].get("files")
+    t_files = data[b"info"].get(b"files")
 
-    piece_count = int(len(data["info"]["pieces"]) / 20)
+    piece_count = int(len(data[b"info"][b"pieces"]) / 20)
     if t_files:
         for file in t_files:
-            file_path = base_save_path.joinpath(*[p.decode() for p in file["path"]])
+            file_path = base_save_path.joinpath(*[p.decode() for p in file[b"path"]])
             if not file_path.exists():
                 files.append(
                     {"complete": 0, "mtime": 0, "priority": un_complete_file_prop}
@@ -123,20 +123,20 @@ def __add_resume_file(
             )
     else:
         try:
-            stat = base_save_path.joinpath(data["info"]["name"].decode()).lstat()
+            stat = base_save_path.joinpath(data[b"info"][b"name"].decode()).lstat()
         except FileNotFoundError:
             return torrent_content
 
-        if stat.st_size == data["info"]["length"]:
+        if stat.st_size == data[b"info"][b"length"]:
             files.append(
                 {"complete": piece_count, "mtime": int(stat.st_mtime), "priority": 1}
             )
         else:
             return torrent_content
 
-    data["rtorrent"] = None
-    del data["rtorrent"]
+    data[b"rtorrent"] = None
+    del data[b"rtorrent"]
 
-    data["libtorrent_resume"] = {"bitfield": piece_count, "files": files}
+    data[b"libtorrent_resume"] = {"bitfield": piece_count, "files": files}
 
     return bencode2.bencode(data)
